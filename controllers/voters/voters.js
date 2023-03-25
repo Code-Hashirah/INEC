9af9111149df2d1882b414c823b98a6c1f11abd5
Kabir <kabirajibade@yahoo.com>
2023-03-25 22:26:42 +0100

    working on registration and account activation

--- controllers/voters/voters.js
+++ controllers/voters/voters.js
@@ -1,14 +1,75 @@
 const Voter = require('../../models/voters/voter')
 const session= require('express-session')
 const bcrypt=require('bcrypt')
-const {valiadtionResult}=require('express-validator/check')
+const {validationResult}=require('express-validator/check')
 const nodemailer=require('nodemailer')
-
+const crypto=require('crypto')
 exports.registerVoterPage=(req,res)=>{
-    res.render('/voters/registration', {title:"We-Vote::Register"})
+    let errors=req.flash('errors');
+    res.render('voters/registration', {title:"We-Vote::Register", ErrorMsg:errors})
 }
 
 exports.registerVoter=(req,res)=>{
 const {Email, Nin, Name, Phone, Password}=req.body
+let Active="NO";
+let errors=validationResult(req)
+if(!errors.isEmpty()){
+    req.flash('errors', errors.array())
+    return req.session.save(()=>{
+        return res.redirect('/register')
+    })
+}
+crypto.randomBytes(32, (err, buffer)=>{
+    if(err){
+        req.flash('userErr', 'Unable to perfom this action')
+        req.session.save(()=>{
+            res.redirect('/register')
+        })
+    }
+    let token=buffer.toString('hex');
+    bcrypt.hash(Password, 12).then(hashedPassword=>{
+        Voter.create({
+            email:Email,
+            nin:Nin,
+            name:Name,
+            phone:Phone,
+            password:hashedPassword,
+            active:Active
+        }).then(voter=>{
+            // voter.resetToken=token;
+            // voter.resetTokenExpiration=Date.now() +90000000
+            const email={
+                to:[user.email, 'newuser@wevote.com'],
+                from:{
+                    name: 'We- Vote',
+                    email:'info@wevote.com.ng'
+                },
+                subject:'Welcome' +user.name+' thank you for registering',
+                html:`
+                <h2> Activate your account </h2>
+                <p> <a href="http:/localhost:3005/activate/${token}"> Activate</a> </p>
+                `
+            }
+            var transport=nodemailer.createTransport({
+                host:"sandbox.smtp.mailtrap.io",
+                port:2525,
+                auth:{
+                    user: "58b42af4ae2024",
+                    pass: "822d08109dd72d"
+                }
+            })
+            transport.sendMail(email).then((response)=>{
+                res.redirect('/')
+            }).catch(err=>{
+                console.log(err)
+            })
+        }).catch(err=>{
+            console.log(err)
+        })
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
 
 }
--- routes/voters/voterRoute.js
+++ routes/voters/voterRoute.js
@@ -1,9 +1,25 @@
+const voterController=require('../../controllers/voters/voters');
 const router=require('express').Router();
-const resultController=require('../../controllers/voters/voters');
+const {isEmpty}=require('validator');
+const {check}=require('express-validator')
+
 const page=require('../../controllers/page')
 
 router.get('/',page.homePage)
-
+router.get('/register',voterController.registerVoterPage)
+router.post('/register', [
+check('Email').notEmpty().withMessage('Email cannot be blank').isEmail().withMessage('Invalid Email').normalizeEmail(),
+check('Nin').notEmpty().withMessage('NIN must be filled').isNumeric().withMessage('Invalid NIN'),
+check('Name').notEmpty().withMessage('Name cannot be empty'),
+check('Phone').notEmpty().withMessage('Phone number is required').isNumeric('Invalid input'),
+check('Password').notEmpty().withMessage('Password cannot be empty').isLength({min:6}).withMessage('Password must be more than 6 characters long'),
+check('ConfirmPass').notEmpty().withMessage('This field cannot be empty').custom((value, {req})=>{
+        if(value !==req.body.Password){
+            throw new Error ('The Password do not match');
+        }
+        return true;
+})
+],voterController.registerVoter)
 
 
 module.exports=router;
